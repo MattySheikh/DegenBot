a703c82666b39678f81ba500453afafe2db54705
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2024-04-04 22:01:26 -0700

    SC and SWI pins and clears

--- commands/sc.ts
+++ commands/sc.ts
@@ -1,6 +1,21 @@
 import { Message} from 'discord.js';
 
 export const execute = async (params: string, message: Message) => {
-	const { channel } = message;
-	channel.send('CLEAR');
+	// Make sure the bot has pin permissions in the server lol
+	try {
+		const { channel } = message;
+		const time = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+		const msg = await channel.send(`Last clear: ${time}`);
+		const currentPinned = await channel.messages.fetchPinned();
+		for (const [, curr] of [...currentPinned]) {
+			const { content } = curr;
+			if (content.startsWith('Last clear: ') || content.startsWith('SWI: ')) {
+				curr.unpin();
+			}
+		}
+
+		await msg.pin();
+	} catch (e) {
+		console.log('UNABLE TO PIN/SEND SC MESSAGE', e);
+	}
 }
--- commands/swi.ts
+++ commands/swi.ts
@@ -1,6 +1,28 @@
 import { Message} from 'discord.js';
 
 export const execute = async (params: string, message: Message) => {
-	const { channel } = message;
-	channel.send(`https://www.neopets.com/objects.phtml?obj_type=${params}&type=shop`);
+	try {
+		const { channel } = message;
+		const msg = await channel.send(`SWI: <https://www.neopets.com/objects.phtml?obj_type=${params}&type=shop>`);
+
+		const currentPinned = await channel.messages.fetchPinned();
+
+		let notPinnedYet = true;
+		for (const [, curr] of [...currentPinned]) {
+			const { content } = curr;
+			if (content.startsWith('SWI: ')) {
+				const currObj = (content.match('type=(.*)&type') || [])[1];
+				if (currObj === params) {
+					notPinnedYet = false;
+					break;
+				}
+			}
+		}
+
+		if (notPinnedYet) {
+			await msg.pin();
+		}
+	} catch (e) {
+		console.log('UNABLE TO PIN/SEND SWI MESSAGE', e);
+	}
 }
